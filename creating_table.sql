2f8a9a5b2d86b6fe19eb6d8c779811da39d340a2
Sumayamd <124234075+Sumayamd@users.noreply.github.com>
2023-02-15 15:39:09 +0530

    addedfile

---
+++ creating_table.sql
@@ -0,0 +1,10 @@
+here is a sample data can creat in lone table
+
+| ID      |    First Name         | Last Name    |          Email              |    Loan Amount    |   purpose          |  Status    |
+|---      |---                    |      ---     |   ---                       |     ---           |   ---              |  ---       |
+|1        |    John               |      Doe     |  John.doe@example.com       |      1000         |  Home improvement  | Approved   |
+|2        |    Jene               |      Smith   |  jene.smith@example.com     |      5000         |  Debt Consolidation| Approved   |
+|3        |    Bob                |      Johnson |  bob.johnson@example.com    |      15000        |  Business          | pending    |
+|4        |    Emily              |      Brown   |  emily.brown@example.com    |      8000         |  Vacation          | Approved   |
+|5        |    Michael            |      Davis   |  Michael.davis@example.com  |      20000        |  Education         | Denied     |
+|6        |    Sarah              |      Wilson  |  sarah.Wilson@example.com   |      12000        |  Car Purchase      | Approved   |
